83a151e80b5176e8715ea0d2732145854ddcdcbc
Nico <citrouille86@gmail.com>
2018-03-01 14:21:54 +0100

    Update suite 1er training

--- src/test/java/fr/apside/demo/util/AddressUtilsTest.java
+++ src/test/java/fr/apside/demo/util/AddressUtilsTest.java
@@ -1,7 +1,9 @@
 package fr.apside.demo.util;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import fr.apside.demo.domain.Address;
@@ -11,12 +13,40 @@ import fr.apside.demo.domain.datagouv.SearchResponse;
 
 public class AddressUtilsTest {
 
+    private SearchResponse searchResponseWithTwoResults;
+
+    @Before
+    public void init() {
+
+        searchResponseWithTwoResults = new SearchResponse();
+
+        Feature features[] = { new Feature(), new Feature() };
+        // First response
+        Properties propertiesOne = new Properties();
+        propertiesOne.setHousenumber("2");
+        propertiesOne.setStreet("Place de la Gare");
+        propertiesOne.setPostcode("37700");
+        propertiesOne.setCity("Saint-Pierre-des-Corps");
+        propertiesOne.setScore(0.9d);
+        // Second response
+        Properties propertiesTwo = new Properties();
+        propertiesTwo.setHousenumber("2");
+        propertiesTwo.setStreet("Avenue de Paris");
+        propertiesTwo.setPostcode("45000");
+        propertiesTwo.setCity("Orléans");
+        propertiesTwo.setScore(0.8d);
+
+        features[0].setProperties(propertiesOne);
+        features[1].setProperties(propertiesTwo);
+
+        searchResponseWithTwoResults.setFeatures(features);
+
+    }
+
     @Test
     public void should_return_properties_from_searchResponse_with_highest_score() {
 
-        SearchResponse searchResponse = createSearchResponseExampleWithTwoResults();
-
-        Properties result = AddressUtils.getPropertiesWithBestScoreFromSearchResponse(searchResponse);
+        Properties result = AddressUtils.getPropertiesWithBestScoreFromSearchResponse(searchResponseWithTwoResults);
 
         assertEquals("Saint-Pierre-des-Corps", result.getCity());
     }
@@ -24,7 +54,7 @@ public class AddressUtilsTest {
     @Test
     public void should_return_address_from_properties() {
 
-        Properties properties = createPropertiesExample();
+        Properties properties = searchResponseWithTwoResults.getFeatures()[0].getProperties();
 
         Address result = AddressUtils.getAddressFromProperties(properties);
 
@@ -34,35 +64,15 @@ public class AddressUtilsTest {
         assertEquals("Saint-Pierre-des-Corps", result.getCity());
     }
 
-    private Properties createPropertiesExample() {
-        return createSearchResponseExampleWithTwoResults().getFeatures()[0].getProperties();
-    }
+    @Test
+    public void should_return_inline_address() {
+        Address addressExample = new Address();
+        addressExample.setNumber("2");
+        addressExample.setStreet("Place de la Gare");
+        addressExample.setPostcode("37700");
+        addressExample.setCity("Saint-Pierre-des-Corps");
 
-    private SearchResponse createSearchResponseExampleWithTwoResults() {
-
-        SearchResponse searchResponse = new SearchResponse();
-
-        Feature features[] = { new Feature(), new Feature() };
-
-        Properties propertiesOne = new Properties();
-        propertiesOne.setHousenumber("2");
-        propertiesOne.setStreet("Place de la Gare");
-        propertiesOne.setPostcode("37700");
-        propertiesOne.setCity("Saint-Pierre-des-Corps");
-        propertiesOne.setScore(0.9);
-
-        Properties propertiesTwo = new Properties();
-        propertiesTwo.setHousenumber("2");
-        propertiesTwo.setStreet("Avenue de Paris");
-        propertiesTwo.setPostcode("45000");
-        propertiesTwo.setCity("Orléans");
-        propertiesTwo.setScore(0.8);
-
-        features[0].setProperties(propertiesOne);
-        features[1].setProperties(propertiesTwo);
-
-        searchResponse.setFeatures(features);
-
-        return searchResponse;
+        assertThat(AddressUtils.createInlineAddressFromObject(addressExample))
+                .isEqualTo("2 Place de la Gare 37700 Saint-Pierre-des-Corps");
     }
 }
